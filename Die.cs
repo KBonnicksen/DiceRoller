26ad821fcdbc04effa9d962c77a77d6d704afcba
KBonnicksen <kelseybon@gmail.com>
2019-07-08 18:39:43 -0700

    Added readonly to prevent numberOfSides in Die class from being changed

--- Die.cs
+++ Die.cs
@@ -12,12 +12,67 @@ namespace DiceRoller
     class Die
     {
         private byte faceValue;
-        private bool isHeld;
         private byte numberOfSides;
+        private static Random rand;
 
+        static Die()
+        {
+            //You will never call the static class in code, it 
+            //will be called during runtime
+            rand = new Random();
+        }
+
+        /// <summary>
+        /// Creates a standart 6-sided die
+        /// </summary>
+        public Die() : this(6)
+        {
+        }
+
+        /// <summary>
+        /// Creates a die with the specified number of sides
+        /// </summary>
+        /// <param name="numberOfSides"></param>
+        public Die(byte numberOfSides)
+        {
+            this.numberOfSides = numberOfSides;
+            Roll();
+        }
+
+        public byte FaceValue
+        {
+            get { return faceValue; }
+            private set //Because you only want to be able to set it within the class
+            {
+                if(value == 0)
+                    throw new Exception("Die cannot be 0");
+
+                faceValue = value;
+            }
+        }
+
+        public bool IsHeld { get; set; }
+ 
+        /// <summary>
+        /// Rolls the die, sets face value to generated number 
+        /// and returns generated number
+        /// </summary>
         public byte Roll()
         {
-            throw new NotImplementedException();
+            //if the die is held, return current face value
+            if (IsHeld)
+            {
+                return FaceValue;
+            }
+            const byte MinDieValue = 1;
+
+            //offset because of exclusice upper bound for random
+            byte MaxDieValue = (byte)(numberOfSides + 1);
+
+            byte result = (byte)rand.Next(MinDieValue, MaxDieValue);
+            FaceValue = result;
+
+            return result;
         }
     }
 }
